b961ca2809f9aac2e08796de2d0721b80cd1fbf1
Gabriel <gfigueiras28@gmail.com>
2025-10-28 10:00:02 -0300

    Arquivo de Back End

---
+++ Agencia/Program.cs
@@ -0,0 +1,219 @@
+﻿int opcao = -1;
+int totalClientes = 0;
+string[] nome = new string[4];
+double[] saldos = new double[4];
+int totalSaldos = 0;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"========= SISTEMA BANCARIO SIMPLES ==========");
+    Console.WriteLine();
+
+    Console.WriteLine($"1- Cadastrar Cliente");
+    Console.WriteLine($"2- Depositar");
+    Console.WriteLine($"3- Sacar");
+    Console.WriteLine($"4- Transferir");
+    Console.WriteLine($"5- Listar Cliente");
+    Console.WriteLine($"0- Sair");
+
+    Console.Write($"Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine($"Encerrando ...");
+            Console.WriteLine($"Pressione <Enter> para encerrar ...");
+            Console.ReadLine();
+            break;
+
+        case 1:
+            CadastrarCliente();
+            Console.WriteLine();
+            break;
+
+
+        case 2:
+            Depositar();
+
+            
+            break;
+
+
+        case 3:
+            Sacar();
+            Console.WriteLine();
+            break;
+
+
+        case 4:
+            Transferir();
+            Console.WriteLine();
+            break;
+
+
+        case 5:
+            ListarCliente();
+            break;
+
+
+        default:
+            Console.WriteLine($"Opção invalida, pressione <Enter> para continuar");
+            break;
+    }
+
+Console.WriteLine($"Clique em <Enter> para continuar");
+    Console.ReadLine();
+
+
+} while (opcao != 0);
+
+
+
+
+
+void CadastrarCliente()
+{
+    if (totalClientes >= 3)
+    {
+        Console.WriteLine($"Limite de Ocupação atingido");
+        return;
+    }
+
+    Console.WriteLine($"Digite o nome do cliente");
+    string n = Console.ReadLine();
+    nome[totalClientes] = n;
+    saldos[totalClientes] = 0;
+    totalClientes++;
+
+}
+
+
+
+int BuscarCliente()
+{
+    ListarCliente();
+    Console.WriteLine($"Digite o numero do cliente: ");
+    int idCliente = int.Parse(Console.ReadLine());
+
+    if (idCliente < 0 || idCliente >= totalClientes)
+    {
+        Console.WriteLine($"Cliente não encontrado");
+        return -1;
+    }
+
+    return idCliente;
+}
+
+
+
+
+void Depositar()
+{
+    if (totalSaldos >= 4)
+    {
+        Console.WriteLine($"Limite de Ocupação atingido");
+        return;
+    }
+
+    int idCliente = BuscarCliente();
+    if (idCliente == -1)
+    {
+        return;
+    }
+
+    Console.WriteLine($"Digite quanto deseja depositar? ");
+    double s = double.Parse(Console.ReadLine());
+
+    saldos[idCliente] += s;
+
+    Console.WriteLine($"Deosito de R$ {s} realizado !!!");
+}
+
+
+
+
+
+void Sacar()
+{
+    if (totalSaldos >= 4)
+    {
+        Console.WriteLine($"Limite de Ocupação atingido");
+        return;
+    }
+
+    int idCliente = BuscarCliente();
+    if (idCliente == -1)
+    {
+        return;
+    }
+
+    Console.WriteLine($"Digite quanto deseja sacar? ");
+    double valor = double.Parse(Console.ReadLine());
+
+    if (saldos [idCliente] >= valor )
+    {
+        saldos[idCliente] -= valor;
+        Console.WriteLine($"Saque Realizado com sucesso !!!");
+    } else
+    {
+        Console.WriteLine($"Saldo insuficiente");
+    }
+
+
+}
+
+
+
+
+void Transferir()
+{
+    Console.WriteLine($"==   TRANSFERENCIA ==");
+
+    Console.WriteLine($"Conta de Origem");
+    int idClienteOrigem = BuscarCliente();
+    if (idClienteOrigem == -1)
+    {
+        return;
+    }
+
+    Console.WriteLine($"Conta de Destino");
+    int idClienteDestino = BuscarCliente();
+    if (idClienteDestino == -1)
+    {
+        return;
+    }
+
+    Console.WriteLine($"Valor para transferir: ");
+    double valorTransferencia = double.Parse(Console.ReadLine());
+
+
+    if (saldos[idClienteOrigem] >= valorTransferencia)
+    {
+        saldos[idClienteOrigem] -= valorTransferencia;
+        saldos[idClienteDestino] += valorTransferencia;
+        Console.WriteLine($"Transferencia Realizada !!!");
+    } else
+    {
+        Console.WriteLine($"Saldo insuficiente !!! ");
+    }
+
+}
+
+
+
+
+void ListarCliente()
+{
+
+    Console.WriteLine();
+    Console.WriteLine($"======= LISTA DE CLIENTES =======");
+
+    for (int i = 0; i < totalClientes; i++)
+    {
+        Console.WriteLine($"Nome: {nome[i]} | saldo: R$ {saldos[i]}");
+        Console.WriteLine();
+    }
+}
---
+++ ReviewArrays/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Reflection.PortableExecutable;
+
+string nome = "Gabriel";
+Console.WriteLine($"Nome: {nome}");
+
+string[] nomes = new string[4];
+int[] idades = new int[4];
+int opcao = -1;
+int totalAlunos = 0;
+
+/*nomes[0] = "Maria";
+idades[0] = 20;
+nomes[1] = "Matheus";
+idades[1] = 16;
+nomes[2] = "Eloysa";
+idades[2] = 17;
+nomes[3] = "Felipe";
+idades[3] = 17;
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine(nomes[0]);
+Console.WriteLine(nomes[1]);
+Console.WriteLine(nomes[2]);
+Console.WriteLine(nomes[3]);*/
+
+do
+{
+    Console.Clear();
+    Console.WriteLine($"1) Cadastra Alunos");
+    Console.WriteLine($"2) Listar Alunos");
+    Console.WriteLine($"0) Sair");
+    Console.Write($"Digite uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+
+    switch (opcao)
+    {
+        case 0:
+            Console.WriteLine($"Encerrando ...");
+            Console.WriteLine($"Pressione <Enter> para encerrar ...");
+            Console.ReadLine();
+            break;
+
+        case 1:
+            CadastrarAlunos();
+            break;
+
+        case 2:
+            ListarAlunos();
+            break;
+
+
+        default:
+            Console.WriteLine($"Opção invalida, pressione <Enter> para continuar");
+            break;
+    }
+
+
+
+} while (opcao != 0);
+
+
+
+
+void CadastrarAlunos()
+{
+    Console.WriteLine($"Digite o nome do aluno");
+    string n = Console.ReadLine();
+    Console.WriteLine($"Digite a idade de {n}");
+    int i = int.Parse(Console.ReadLine());
+
+    nomes[totalAlunos] = n;
+    idades[totalAlunos] = i;
+    totalAlunos++;
+
+    Console.WriteLine($"Aluno cadastrado com sucesso");
+    Console.WriteLine($"Há {totalAlunos} alunos(s) cadastrado(s) no sistema!");
+    Console.WriteLine($"Pressione uma tecla para continuar");
+    Console.ReadLine();
+
+    if (totalAlunos >= 4)
+    {
+        Console.WriteLine($"Não Limite de ocupação atingido");
+        return;
+    }
+}
+
+
+
+
+void ListarAlunos()
+{
+
+    Console.WriteLine();
+    Console.WriteLine($"Resultado: ");
+
+
+    for (int i = 0; i < nomes.Length; i++)
+    {
+        Console.WriteLine(nomes[i]);
+        Console.WriteLine($" Nome: {nomes[i]}");
+        Console.WriteLine($"Idade: {idades[i]} anos");
+        Console.WriteLine();
+    }
+    Console.WriteLine($"");
+
+}
+
